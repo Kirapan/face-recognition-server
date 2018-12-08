62e267803730127e6d21cf9b29e81c3eed102506
Kirapan <pxqpxqpxq@hotmail.com>
2018-12-08 11:15:42 -0500

    fixed database

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ const app = express();
 app.use(bodyParser.json())
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {res.send('it is working!')})
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
@@ -31,6 +30,6 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`app is running on port ${process.env.PORT}`)
 })
