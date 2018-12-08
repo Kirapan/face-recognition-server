7518a07df828cddbd2e1bcb7d775458534c0d04d
Kirapan <pxqpxqpxq@hotmail.com>
2018-12-08 11:45:17 -0500

    change entries

--- controllers/image.js
+++ controllers/image.js
@@ -14,16 +14,14 @@ const handleApiCall = (req, res) => {
 }
 
 const handleImage = async (req,res, db) => {
-  try {
     const { id } =  req.body;
-    const response = await db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    res.json(response[0])
-  }
-  catch(err) {
-    res.status(400).json('unable to get entries')
-  }
+    db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
 }
 
 module.exports = {
