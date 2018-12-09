5f089ee8663dcd19bce272442451e7c00c9d7e06
Kirapan <pxqpxqpxq@hotmail.com>
2018-12-08 21:36:10 -0500

    log error

--- controllers/image.js
+++ controllers/image.js
@@ -14,14 +14,17 @@ const handleApiCall = (req, res) => {
 }
 
 const handleImage = async (req,res, db) => {
+  try {
     const { id } =  req.body;
-    db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+    const response = await db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    res.json(response[0])
+  }
+  catch(err) {
+    console.log(err);
+    res.status(400).json('unable to get entries')
+  }
 }
 
 module.exports = {
